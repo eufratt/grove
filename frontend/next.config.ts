05f6b6ef020af17ad97d316d01b5847700a453ac
eufratt <ayyasheufrat2@gmail.com>
2026-07-28 20:49:53 +0700

    feat: add supabase storage hostname to next image config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -12,6 +12,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'pckippyllhtzzveehfjr.supabase.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'sbdnleyvijenwzikgyml.supabase.co',
+      },
     ],
   },
   async headers() {
